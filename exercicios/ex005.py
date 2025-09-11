e2f102eede8f5ec3058d9b17af2983cf158a93e0
rlzb <kalyeltonvieira@gmail.com>
2025-09-11 14:11:57 -0300

    update

---
+++ exercicios/ex005.py
@@ -0,0 +1,6 @@
+numero = int(input("Digite Um Numero:"))
+antecessor = numero -1
+sucessor = numero +1
+
+print(f"o antecessor de {numero} sera {antecessor}")
+print(f"o sucessor de {numero} sera {sucessor}")
--- projetos/calculadora.py
+++ projetos/calculadora.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 from tkinter import messagebox
 
 # Criar a janela principal
@@ -6,23 +7,23 @@ janela = tk.Tk()
 janela.title("Meu Primeiro Programa")
 janela.geometry("400x300")
 
-def clicou_botao():
+def usuario():
     nome = entrada_nome.get()
+    lista_nomes = ["julia", "maria", "joaquim"]
+    if lista_nomes == nome:
+        nome.random(lista_nomes)
     if nome:
-        messagebox.showinfo("Mensagem" f"ola, {nome}! Bem Vindo")
+        messagebox.showinfo("Meu Primeiro Software", f"BEM VINDO {lista_nomes}")
     else:
-        messagebox.showwarning("Aviso.", f"Por Favor, Digite Seu nome.")
+        messagebox.showwarning("ERRO", "INSIRA SEUS DADOS CORRETAMENTE")
     
-rotulo = tk.Label(janela, text="Digite Seu Nome:", font=("Arial", 14))
+rotulo = tk.Label(janela, text="DIGITE SEU NOME:", font=("Arial", 12))
 rotulo.pack(pady=10)
 
-entrada_nome = tk.Button(janela, text="Clique Aqui!", command=clicou_botao, bg="lightblue", font=("Arial", 12))
+entrada_nome = tk.Entry(janela, font=("Arial", 15))
 entrada_nome.pack(pady=5)
-
-botao = tk.Button(janela, text="", font=("Arial", 10))
+lista_nomes = tk.Label(janela, background="black", font=("Arial", 12))
+lista_nomes.pack(pady=10)
+botao = tk.Button(janela, text="Clique Aqui", command=usuario, bg="green", font=("SansSerif", 12))
 botao.pack(pady=10)
-
-mensagem = tk.Label(janela, text="", font=("Arial", 10))
-mensagem.pack(pady=10)
-
 janela.mainloop()
