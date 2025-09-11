39be724ac821ef49fda578a5bbbc78a3179f1049
rlzb <kalyeltonvieira@gmail.com>
2025-09-11 16:25:38 -0300

    Update ex009.py

--- exercicios/ex009.py
+++ exercicios/ex009.py
@@ -1,6 +1,25 @@
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
 print("=" * 23)
-print("| BEM VINDO A TABUADA |")
+print("|                     |")
+print(Fore.GREEN + "| BEM VINDO A TABUADA |")
+print("|                     |")
 print("=" * 23)
 
 numero = int(input("DIGITE UM NUMERO E VEJA A TABUADA: "))
-valor = numero * numero + 
+print("=" * 14)
+print("{} x {} = {}".format(numero, 1, numero * 1))
+print("{} x {} = {}".format(numero, 2, numero * 2))
+print("{} x {} = {}".format(numero, 3, numero * 3))
+print("{} x {} = {}".format(numero, 4, numero * 4))
+print("{} x {} = {}".format(numero, 5, numero * 5))
+print("{} x {} = {}".format(numero, 6, numero * 6))
+print("{} x {} = {}".format(numero, 7 , numero * 7))
+print("{} x {} = {}".format(numero, 8, numero * 8))
+print("{} x {} = {}".format(numero, 9, numero * 9))
+print("{} x {} = {}".format(numero, 10, numero * 10))
+print("=" * 14)
+
+janela.mainloop()
