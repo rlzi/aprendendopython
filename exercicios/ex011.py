4e4bb31c9360b4329c2757b2744aaef2c6b0dc87
rlzb <kalyeltonvieira@gmail.com>
2025-09-11 17:02:14 -0300

    Create ex011.py

---
+++ exercicios/ex011.py
@@ -0,0 +1,6 @@
+larg = float(input("Digite a Largura da Parede: "))
+alt = float(input("Digite a Altura da Parede: "))
+area = larg * alt
+print(f"Sua Parede Tem Dimensao de {larg} x {alt} e sua Area e De {area}m2")
+tinta = area / 2
+print(f"Para Pintar essa Parede, voce precisara de {tinta}L de tinta")
