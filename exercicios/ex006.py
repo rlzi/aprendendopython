07ecce383362e544887bae416b76e5b116117c6b
rlzb <kalyeltonvieira@gmail.com>
2025-09-11 14:17:57 -0300

    update

---
+++ exercicios/ex006.py
@@ -0,0 +1,8 @@
+import math
+valor = int(input("Digite Um Valor: "))
+dobro = valor * 2
+triplo = valor * 3
+raiz = math.sqrt(valor)
+print(f"O Dobro de {valor} Sera {dobro}")
+print(f"O Triplo de {valor} Sera {triplo}")
+print(f"A Raiz Quadrada de {valor} sera {raiz}")
