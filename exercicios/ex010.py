b0f3e107377a09a7247d3f72a3c5a792a3a6fb5f
rlzi <kalyeltonvieira@gmail.com>
2025-10-26 18:40:55 -0300

    Update ex010.py

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,3 +1,3 @@
-real = float(input("QUANTOS REAIS VOCE TEM NA CARTEIRA? R$"))
+real = float(input("\033[94mQUANTOS REAIS VOCE TEM NA CARTEIRA? R$\033[0m"))
 dolar = real / 5.39
-print(f"COM R${real} VOCE PODE COMPRAR US${dolar}")
+print(f"COM \033[92mR${real}\033[0m VOCE PODE COMPRAR \033[91mUS${dolar:.0f}\033[0m")
