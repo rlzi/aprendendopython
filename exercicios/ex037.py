a19dc2e115eac3fc2897da86b2db6e084d552e60
rlzi <kalyeltonvieira@gmail.com>
2025-11-26 22:44:05 -0300

    Update ex037.py

--- exercicios/ex037.py
+++ exercicios/ex037.py
@@ -1,14 +1,26 @@
-n = int(input("Digite um numero inteiro: "))
-print("""ESCOLHA UMA DAS BASES PARA CONVERTER
----------------------------------
-[1] CONVERTER PARA BINARIO
-[2] CONVERTER PARA OCTAL
-[3] CONVERTER PARA HEXADECIMAL
----------------------------------""")
-opcao = int(input("SUA OPCAO: "))
+from colorama import Fore, Back, Style
+print(Fore.GREEN +"--------------------------------------------")
+print("    BEM VINDO AO CONVERSOR DE VALORES")
+print("--------------------------------------------")
+n = int(input("DIGITE UM NUMERO: "))
+print("--------------------------------------------" + Fore.RESET)
+
+print(Fore.BLUE + """
+=================================================
+[1] BINARIO
+[2] OCTAL
+[3] HEXADECIMAL
+=================================================""" + Fore.RESET)
+
+opcao = int(input(Fore.BLACK + "ESCOLHA A OPCAO PARA CONVERTER: "+ Fore.RESET)) #escolher para qual converter
+
+#opcoes
+
 if opcao == 1:
-    print(f"{n} CONVERTIDO PARA BINARIO E IGUAL A {bin(n)[2:]}")
+    print(f"O NUMERO {n} CONVERTIDO PARA BINARIO SERA {bin(n)}")
 elif opcao == 2:
-    print(f"{n} CONVERTIDO PARA OCTAL E IGUAL A {oct(n)[2:]}")
+    print(f"O NUMERO {n} CONVERTIDO PARA OCTAL SERA {oct(n)}")
 elif opcao == 3:
-    print(f"{n} CONVERTIDO PARA HEXADECIMAL E IGUAL A {hex(n)[2:]}")
+    print(f"O NUMERO {n} CONVERTIDO PARA HEXADECIMAL SERA {hex(n)}")
+else:
+    print("OPCAO INVALIDA")
