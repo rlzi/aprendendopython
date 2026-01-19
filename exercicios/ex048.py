03fbadfefc735dc12a885b075a3ecf08ddfbd3f9
rlzi <kalyeltonvieira@gmail.com>
2026-01-19 00:19:38 -0300

    Update ex048.py

--- exercicios/ex048.py
+++ exercicios/ex048.py
@@ -3,4 +3,4 @@ for c in range(1,501, 2):
     if c % 3 == 0:
         soma = soma + c
     
-print(f'A soma de todos valores solicitados sao {soma}') 
+print(f'A soma de todos valores solicitados sao {soma}') # here we have the sum of all the values
