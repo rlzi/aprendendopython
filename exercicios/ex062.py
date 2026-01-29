391fd67f5644806b27469fac37901bd3106f7ceb
rlzi <kalyeltonvieira@gmail.com>
2026-01-29 09:12:03 -0300

    Update ex062.py

--- exercicios/ex062.py
+++ exercicios/ex062.py
@@ -1,13 +1,20 @@
+print('=' * 10)
 print('Gerador de PA')
-print('-=' * 10)
+print('=' * 10)
 
 primeiro = int(input('Primeiro Termo: '))
 razao = int(input('Razao da PA: '))
 termo = primeiro
 cont = 1
-while cont < 10:
-    print(f'{termo} -> ',end='')
-    termo += razao
-    cont +=1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print(f'{termo} -> ', end='')
+        termo += razao
+        cont +=1
+    print('PAUSA')
+    mais = int(input('Quantos termos voce quer mostrar a mais?: '))
 
-print('FIM')
+print('Fim Do Programa.')
