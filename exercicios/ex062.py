3ac4f076e306eacf005a0bbe7ac0e1ce8c18f665
rlzi <kalyeltonvieira@gmail.com>
2026-01-29 08:32:13 -0300

    update

--- exercicios/ex062.py
+++ exercicios/ex062.py
@@ -1,11 +1,12 @@
 print('Gerador de PA')
 print('-=' * 10)
+
 primeiro = int(input('Primeiro Termo: '))
 razao = int(input('Razao da PA: '))
 termo = primeiro
 cont = 1
 while cont < 10:
-    print(f'{termo} -> ', end='')
+    print(f'{termo} -> ',end='')
     termo += razao
     cont +=1
 
