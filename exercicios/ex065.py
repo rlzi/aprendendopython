8222d27788ce7afd6d96e0ac85769f4f2c565cad
rlzi <kalyeltonvieira@gmail.com>
2026-01-29 19:01:15 -0300

    Update ex065.py

--- exercicios/ex065.py
+++ exercicios/ex065.py
@@ -1,11 +1,18 @@
 resp = "S"
-soma = quant = media = 0
+soma = quant = media = maior = menor = 0
 while resp in 'Ss':
     num = int(input('Digite um numero: '))
     soma += num
     quant += 1
-    
-    resp = str(input('Quer Continuar?: [S/N]')).upper().strip()[0]
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input('Quer Continuar?: [S/N] ')).upper().strip()[0]
 media = soma / quant
 print(f'Voce digitou {quant} e a media foi {media}')
+print(f'O maior numero foi {maior} o menor numero foi {menor}')
 print('Fim Do Programa')
