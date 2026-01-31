8b20925ec2d0e5fa4ed99b891067f6b5179b8e52
rlzi <kalyeltonvieira@gmail.com>
2026-01-31 10:05:24 -0300

    Update ex067.py

--- exercicios/ex067.py
+++ exercicios/ex067.py
@@ -1,8 +1,18 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-dados = {'categoria':['A', 'B','C'],
-        'total':[300, 180, 9940]}
-df = pd.DataFrame(dados)
-df.plot(kind='bar', x='categoria', y='total')
-plt.show()
+numero = int(input("Digite Um numero Para Ver a Tabuada: "))
+while True:
+    print("=" * 40)
+    print(f"{numero} x 1 = {numero*1}")
+    print(f"{numero} x 2 = {numero*2}")
+    print(f"{numero} x 3 = {numero*3}")
+    print(f"{numero} x 4 = {numero*4}")
+    print(f"{numero} x 5 = {numero*5}")
+    print(f"{numero} x 6 = {numero*6}")
+    print(f"{numero} x 7 = {numero*7}")
+    print(f"{numero} x 8 = {numero*8}")
+    print(f"{numero} x 9 = {numero*9}")
+    print(f"{numero} x 10 = {numero*10}")
+    print("=" * 40)
+    numero = int(input("Quer ver a tabuada de qual valor?: "))
+    if numero <0:
+        break
+print("PROGRAMA DE TABUADA ENCERRADO!!!, volte sempre!")
