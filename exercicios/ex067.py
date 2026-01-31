8ebabaa6fbeef1087ae3d15dc1b14265d1d6bafc
rlzi <kalyeltonvieira@gmail.com>
2026-01-31 10:09:34 -0300

    Update ex067.py

--- exercicios/ex067.py
+++ exercicios/ex067.py
@@ -1,16 +1,8 @@
 numero = int(input("Digite Um numero Para Ver a Tabuada: "))
 while True:
     print("=" * 40)
-    print(f"{numero} x 1 = {numero*1}")
-    print(f"{numero} x 2 = {numero*2}")
-    print(f"{numero} x 3 = {numero*3}")
-    print(f"{numero} x 4 = {numero*4}")
-    print(f"{numero} x 5 = {numero*5}")
-    print(f"{numero} x 6 = {numero*6}")
-    print(f"{numero} x 7 = {numero*7}")
-    print(f"{numero} x 8 = {numero*8}")
-    print(f"{numero} x 9 = {numero*9}")
-    print(f"{numero} x 10 = {numero*10}")
+    for c in range(1,11):
+        print(f"{numero} x {c} = {numero*c}")
     print("=" * 40)
     numero = int(input("Quer ver a tabuada de qual valor?: "))
     if numero <0:
