0efb1f90b5cc6e6ea6ac787e1355f1a62024aea9
rlzi <kalyeltonvieira@gmail.com>
2026-02-01 18:00:15 -0300

    update

--- exercicios/ex070.py
+++ exercicios/ex070.py
@@ -1,4 +1,7 @@
-somatotal = maiorpreco = menorpreco = nomevalor = 0
+from colorama import Fore, init
+
+somatotal = maiorpreco = menorpreco = cont = 0
+barato = 0
 while True:
     nome = str(input("Nome Do Produto: "))
     preco = int(input("Preco: R$"))
@@ -7,14 +10,21 @@ while True:
         maiorpreco +=1
     elif preco <=1000:
         menorpreco +=1
-        nomevalor +=1
+    if cont ==1:
+        menorpreco = preco
+        barato = preco
+    else:
+        if preco < menorpreco:
+            menorpreco = preco
     somatotal += preco
     continuar = str(input("Quer Continuar? [S/N]: ")).strip().upper()[0]
+    while continuar not in "SN":
+        continuar = str(input("Quer Continuar? "))
     if continuar in "N":
         break
     elif continuar in  "S":
         continue
-
+print(Fore.BLUE +"=========INFORMACAO DA COMPRA==========")
 print(f"O Total da compra foi R${somatotal}.00")
 print(f"Temos 1 Produtos custando mais de R$1000.00")
-print(f"O Produto Mais barato foi {nomevalor} que custa R${menorpreco}")
+print(f"O Produto Mais barato foi {barato} que custa R${menorpreco}")
---
+++ exercicios/ex071.py
@@ -0,0 +1,4 @@
+numeros = [1023,5302,2345]
+print(max(numeros))
+print(min(numeros))
+print(bool(numeros))
