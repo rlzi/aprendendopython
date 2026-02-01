21d2c7fd40e94d835c533c432017b366ee31bf4b
rlzi <kalyeltonvieira@gmail.com>
2026-02-01 17:38:19 -0300

    update

---
+++ exercicios/ex070.py
@@ -0,0 +1,20 @@
+somatotal = maiorpreco = menorpreco = 0
+while True:
+    nome = str(input("Nome Do Produto: "))
+    preco = int(input("Preco: R$"))
+    lista = [nome, preco]
+    if preco >= 1000:
+        maiorpreco +=1
+    else:
+        if preco <=1000:
+            menorpreco +=1
+    somatotal += preco
+    continuar = str(input("Quer Continuar? [S/N]: ")).strip().upper()[0]
+    if continuar in "N":
+        break
+    elif continuar in  "S":
+        continue
+
+print(f"O Total da compra foi R${somatotal}.00")
+print(f"Temos 1 Produtos custando mais de R$1000.00")
+print(f"O Produto Mais barato foi  que custa R${menorpreco}")
