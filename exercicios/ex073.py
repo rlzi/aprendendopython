1aef535afcfcf1391cb0d902297e582ce130b886
rlzi <kalyeltonvieira@gmail.com>
2026-02-07 14:06:53 -0300

    Update ex073.py

--- exercicios/ex073.py
+++ exercicios/ex073.py
@@ -1,4 +1,4 @@
-times = ('bragantino', 'Palmeiras', 'Chapecoenses', 'Mirassol'
+times = ('bragantino', 'Palmeiras', 'Chapecoense', 'Mirassol'
         ,'Fluminense', 'Bahia', 'Sao Paulo', 'Botafogo', 'Gremio'
         , 'Athletico-PR', 'Coritiba', 'EC Vitoria', 'Flamengo'
         , 'Atletico-MG', 'Vasco da Gama', 'Internacional'
@@ -12,3 +12,5 @@ print(f'Os 5 Primeiros sao {times[0:5]}')
 print('=-' * 50)
 print(f'Os 4 Ultimos times sao {times[-4:]}')
 print('=-' * 50)
+print(f'\nTimes em ordem alfabetica {sorted(times)}', end=' ')
+print(f'O Chapecoense esta na {times.index("Chapecoense")} posicao')
