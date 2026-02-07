906defb6cb0ce9e4f6c1d3283f0c8d9ba4454423
rlzi <kalyeltonvieira@gmail.com>
2026-02-07 14:15:52 -0300

    Update ex073.py

--- exercicios/ex073.py
+++ exercicios/ex073.py
@@ -1,16 +1,18 @@
+print("=" * 50)
+print("                  TABELA BRASILEIRAO")
 times = ('bragantino', 'Palmeiras', 'Chapecoense', 'Mirassol'
         ,'Fluminense', 'Bahia', 'Sao Paulo', 'Botafogo', 'Gremio'
         , 'Athletico-PR', 'Coritiba', 'EC Vitoria', 'Flamengo'
         , 'Atletico-MG', 'Vasco da Gama', 'Internacional'
         , 'Santos', 'Remo', 'corinthians', 'Cruzeiro')
-
+print("=" * 50)
 for time in times:
-        print('=' * 50)
         print(time)
 print('=' * 50)
 print(f'Os 5 Primeiros sao {times[0:5]}')
 print('=-' * 50)
 print(f'Os 4 Ultimos times sao {times[-4:]}')
 print('=-' * 50)
-print(f'\nTimes em ordem alfabetica {sorted(times)}', end=' ')
-print(f'O Chapecoense esta na {times.index("Chapecoense")} posicao')
+print("==========OS TIMES EM ORDEM ALFABETICA==========")
+print('\n'.join(sorted(times)))
+print(f'O Chapecoense esta na {times.index("Chapecoense")+1} posicao')
