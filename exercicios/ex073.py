56b58f979a010c3a8f3d3bab318d20d739131b48
rlzi <kalyeltonvieira@gmail.com>
2026-02-07 13:50:07 -0300

    Update ex073.py

---
+++ exercicios/ex073.py
@@ -0,0 +1,8 @@
+times = ('bragantino', 'Palmeiras', 'Chapecoenses', 'Mirassol'
+        ,'Fluminense', 'Bahia', 'Sao Paulo', 'Botafogo', 'Gremio'
+        , 'Athletico-PR', 'Coritiba', 'EC Vitoria', 'Flamengo'
+        , 'Atletico-MG', 'Vasco da Gama', 'Internacional'
+        , 'Santos', 'Remo', 'corinthians', 'Cruzeiro')
+
+for time in times:
+    print(time)
